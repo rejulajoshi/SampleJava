6e157a7b483a7df48bff88df9a29f3ad7fc56c25
nkrej <nkrej@RejulasPC>
2024-08-14 11:08:00 +0530

    first commit

---
+++ SampleJava/src/stingBuffer/DeleteOF.java
@@ -0,0 +1,12 @@
+package stingBuffer;
+
+public class DeleteOF {
+
+	public static void main(String[] args) {
+		StringBuffer obj=new StringBuffer("Hello");
+		obj.delete(1, 3);
+		System.out.println(obj);
+
+	}
+
+}
